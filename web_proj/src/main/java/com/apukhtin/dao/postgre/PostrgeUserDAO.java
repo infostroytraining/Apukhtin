e107820bbd7286946d47bef148938caf853c952d
Pandalmail <pandalmail@gmail.com>
2015-12-14 23:42:48 +0200

    Starting implementing PostgreUserDAO

--- web_proj/src/main/java/com/apukhtin/dao/postgre/PostrgeUserDAO.java
+++ web_proj/src/main/java/com/apukhtin/dao/postgre/PostrgeUserDAO.java
@@ -1,22 +1,48 @@
 package com.apukhtin.dao.postgre;
 
+import com.apukhtin.dao.DAOException;
 import com.apukhtin.dao.UserDAO;
+import com.apukhtin.dao.postgre.transaction.TransactionManager;
 import com.apukhtin.model.User;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
+
+import static com.apukhtin.dao.postgre.transaction.TransactionManager.doTransaction;
 
 /**
  * Created by Vlad on 13.12.2015.
  */
 public class PostrgeUserDAO implements UserDAO {
+
+    private final static String SELECT_ALL_QUERY = "SELECT * FROM \"user\"";
+    TransactionManager transactionManager = new TransactionManager();
+
     @Override
     public boolean emailExists(String email) {
         return false;
     }
 
     @Override
-    public List<User> getAll() {
-        return null;
+    public List<User> getAll() throws DAOException {
+        List<User> users = new LinkedList<>();
+        try {
+            ResultSet resultSet = doTransaction((connection -> {
+                return connection.createStatement().executeQuery(SELECT_ALL_QUERY);
+            }));
+
+            while (resultSet.next()) {
+                User user = new User(resultSet.getString("name"), resultSet.getString("password"),
+                        resultSet.getString("email"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            throw new DAOException(e);
+        }
+
+        return users;
     }
 
     @Override
