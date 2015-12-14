6b7df3f213f8d134fca10839d6736db70f2ee9a7
Pandalmail <pandalmail@gmail.com>
2015-12-15 00:18:19 +0200

    add method

--- web_proj/src/main/java/com/apukhtin/dao/postgre/PostrgeUserDAO.java
+++ web_proj/src/main/java/com/apukhtin/dao/postgre/PostrgeUserDAO.java
@@ -2,11 +2,12 @@ package com.apukhtin.dao.postgre;
 
 import com.apukhtin.dao.DAOException;
 import com.apukhtin.dao.UserDAO;
-import com.apukhtin.dao.postgre.transaction.TransactionManager;
 import com.apukhtin.model.User;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -18,7 +19,7 @@ import static com.apukhtin.dao.postgre.transaction.TransactionManager.doTransact
 public class PostrgeUserDAO implements UserDAO {
 
     private final static String SELECT_ALL_QUERY = "SELECT * FROM \"user\"";
-    TransactionManager transactionManager = new TransactionManager();
+    private final static String ADD_QUERY = "INSERT into \"user\"(name, email, password)  VALUES (?,?,?)"; // ? name, ? email, ? password
 
     @Override
     public boolean emailExists(String email) {
@@ -46,8 +47,31 @@ public class PostrgeUserDAO implements UserDAO {
     }
 
     @Override
-    public User add(User obj) {
-        return null;
+    public User add(User obj) throws DAOException {
+        try {
+            int newId = doTransaction(connection -> {
+                PreparedStatement preparedStatement =
+                        connection.prepareStatement(ADD_QUERY, Statement.RETURN_GENERATED_KEYS);
+
+                preparedStatement.setString(1, obj.getName());
+                preparedStatement.setString(2, obj.getEmail());
+                preparedStatement.setString(3, obj.getPassword());
+
+                preparedStatement.executeUpdate();
+
+                int Id = 0;
+                ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    Id = generatedKeys.getInt(1);
+                }
+                return Id;
+            });
+
+            obj.setId(newId);
+            return obj;
+        } catch (SQLException e) {
+            throw new DAOException(e);
+        }
     }
 
     @Override
