61b5240e966cfe170c0257a165c0c78443acc97e
Pandalmail <pandalmail@gmail.com>
2015-12-15 10:55:49 +0200

    End postgreUserDAO

--- web_proj/src/main/java/com/apukhtin/dao/postgre/PostrgeUserDAO.java
+++ web_proj/src/main/java/com/apukhtin/dao/postgre/PostrgeUserDAO.java
@@ -20,10 +20,28 @@ public class PostrgeUserDAO implements UserDAO {
 
     private final static String SELECT_ALL_QUERY = "SELECT * FROM \"user\"";
     private final static String ADD_QUERY = "INSERT into \"user\"(name, email, password)  VALUES (?,?,?)"; // ? name, ? email, ? password
-
+    private final static String CONTAINS_QUERY = "SELECT * FROM \"user\" WHERE id=?";
+    private final static String UPDATE_QUERY = "UPDATE \"user\" SET email=?, name=?, password=? " +
+            "WHERE id = ?";
+    private final static String GET_QUERY = "SELECT * FROM \"user\" WHERE id=?";
+    private final static String REMOVE_QUERY = "DELETE FROM \"user\" WHERE id=?";
+    private final static String EMAIL_EXIST_QUERY = "SELECT * from \"user\" WHERE email=?";
     @Override
-    public boolean emailExists(String email) {
-        return false;
+    public boolean emailExists(String email) throws DAOException {
+        boolean result = false;
+        try {
+            result = doTransaction(connection -> {
+                PreparedStatement statement = connection.prepareStatement(EMAIL_EXIST_QUERY);
+                statement.setString(1, email);
+
+                ResultSet resultSet = statement.executeQuery();
+                return resultSet.next();
+            });
+        } catch (SQLException e) {
+            throw new DAOException(e);
+        }
+
+        return result;
     }
 
     @Override
@@ -75,22 +93,86 @@ public class PostrgeUserDAO implements UserDAO {
     }
 
     @Override
-    public boolean remove(User obj) {
-        return false;
+    public boolean remove(User obj) throws DAOException {
+        if (!contains(obj.getId())) return false;
+        try {
+            return doTransaction(connection -> {
+                PreparedStatement statement = connection.prepareStatement(REMOVE_QUERY);
+                statement.setLong(1, obj.getId());
+
+                return statement.executeUpdate() > 0;
+            });
+        } catch (SQLException e) {
+            throw new DAOException(e);
+        }
+
+    }
+
+    /**
+     * Returns user if db contains it with such id, null otherwise
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public User get(long id) throws DAOException {
+        User user = null;
+        try {
+            if (contains(id)) {
+                ResultSet resultSet = doTransaction(connection -> {
+                    PreparedStatement statement = connection.prepareStatement(GET_QUERY);
+                    statement.setLong(1, id);
+                    return statement.executeQuery();
+                });
+                if (resultSet.next()) {
+                    user = new User();
+
+                    user.setId(id);
+                    user.setEmail(resultSet.getString("email"));
+                    user.setName(resultSet.getString("name"));
+                    user.setPassword(resultSet.getString("password"));
+                }
+            }
+        } catch (SQLException e) {
+            throw new DAOException(e);
+        }
+
+        return user;
     }
 
     @Override
-    public User get(long id) {
-        return null;
+    public void update(User obj) throws DAOException {
+        try {
+            if (contains(obj.getId())) {
+                doTransaction(connection -> {
+                    PreparedStatement statement = connection.prepareStatement(UPDATE_QUERY);
+                    statement.setString(1, obj.getEmail());
+                    statement.setString(2, obj.getName());
+                    statement.setString(3, obj.getPassword());
+
+                    statement.setLong(4, obj.getId());
+                    return statement.executeUpdate();
+                });
+            }
+        } catch (SQLException e) {
+            throw new DAOException(e);
+        }
     }
 
     @Override
-    public void update(User obj) {
+    public boolean contains(long id) throws DAOException {
+        boolean result = false;
+        try {
+            ResultSet resultSet = doTransaction(connection -> {
+                PreparedStatement statement = connection.prepareStatement(CONTAINS_QUERY);
+                statement.setLong(1, id);
+                return statement.executeQuery();
+            });
+            result = resultSet.next();
+        } catch (SQLException e) {
+            throw new DAOException(e);
+        }
 
-    }
-
-    @Override
-    public boolean contains(long id) {
-        return false;
+        return result;
     }
 }
