09c0e81c5b6ba7ded19b6efb489791fd0169ba96
pandalmail <pandalmail@gmail.com>
2015-12-17 20:42:22 +0200

    Logging for services and establisher implemented

--- web_proj/src/main/java/com/apukhtin/dao/connection/ConnectionEstablisher.java
+++ web_proj/src/main/java/com/apukhtin/dao/connection/ConnectionEstablisher.java
@@ -3,6 +3,8 @@ package com.apukhtin.dao.connection;
 import com.apukhtin.dao.connection.configer.ConnectionConfig;
 import com.apukhtin.dao.connection.configer.StringConfigManager;
 import com.apukhtin.dao.connection.configer.exception.ConfigException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -13,6 +15,7 @@ import java.sql.SQLException;
  */
 public class ConnectionEstablisher {
     private static ConnectionConfig connectionConfig;
+    private static Logger logger = LogManager.getLogger();
 
     public static Connection getConnection() throws SQLException {
         StringConfigManager manager =
@@ -21,12 +24,17 @@ public class ConnectionEstablisher {
         Connection connection = null;
         try {
             ConnectionConfig config = manager.getConfig();
+            logger.debug("Config loaded");
+            logger.debug("Try to log driver");
             loadPostrgeDriver();
+            logger.debug("Driver loaded");
             connection =
                     DriverManager.getConnection(config.getUrl(), config.getUser(), config.getPassword());
         } catch (ConfigException e) {
+            logger.catching(e);
             throw new SQLException("Config parametres exception. Details:\n" + e.getMessage(), e);
         } catch (ClassNotFoundException e) {
+            logger.catching(e);
             throw new SQLException("Cannot load driver");
         }
 
@@ -34,6 +42,8 @@ public class ConnectionEstablisher {
     }
 
     private static void loadPostrgeDriver() throws ClassNotFoundException {
+        logger.debug("Trying to load driver org.postgresql.Driver");
         Class.forName("org.postgresql.Driver");
+        logger.debug("Driver succesfully loaded");
     }
 }
--- web_proj/src/main/java/com/apukhtin/services/UserService.java
+++ web_proj/src/main/java/com/apukhtin/services/UserService.java
@@ -4,12 +4,18 @@ import com.apukhtin.dao.DAOException;
 import com.apukhtin.dao.UserDAO;
 import com.apukhtin.dao.memory.InMemoryUserDaoImpl;
 import com.apukhtin.model.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
 
 public class UserService {
 
+    private static Logger logger = LogManager.getLogger();
+
     private UserDAO dao = new InMemoryUserDaoImpl();
 
-    public static void validateForNulls(User user) throws IllegalArgumentException {
+    public static void validateForNulls(User user) throws ServiceException {
+        logger.entry(user);
         boolean isError = false;
         String descr = "";
 
@@ -29,21 +35,27 @@ public class UserService {
         }
 
         if (isError) {
-            throw new IllegalArgumentException(descr);
+            logger.debug("Not validated. " + descr);
+            throw new ServiceException(descr);
         }
     }
 
     public void addUser(User user) throws ServiceException {
+        logger.entry(user);
         validateForNulls(user);
 
         try {
             if (dao.emailExists(user.getEmail())) {
+                logger.warn("User with email " + user.getEmail() + " already exists");
                 throw new ServiceException("User with such email exists");
             }
             dao.add(user);
+            logger.info("User succesfull added", user);
         } catch (DAOException e) {
+            logger.catching(e);
             throw new ServiceException(e);
         }
 
+        logger.exit();
     }
 }
