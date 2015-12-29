e9da18ab9f58a071d97b25c07fa6424e452bd5ef
pandalmail <pandalmail@gmail.com>
2015-12-29 23:43:32 +0200

    100% register servlet test coverage

--- web_proj/src/main/test/com/apukhtin/servlets/RegisterServletTest.java
+++ web_proj/src/main/test/com/apukhtin/servlets/RegisterServletTest.java
@@ -1,6 +1,8 @@
 package com.apukhtin.servlets;
 
-import com.apukhtin.dao.memory.InMemoryUserDaoImpl;
+import com.apukhtin.dao.postgre.PostrgeUserDAO;
+import com.apukhtin.model.User;
+import com.apukhtin.services.ServiceException;
 import com.apukhtin.services.UserService;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +10,7 @@ import org.junit.Test;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 
 import static org.mockito.Mockito.*;
 
@@ -15,7 +18,7 @@ import static org.mockito.Mockito.*;
  * Created by vlad on 26.12.2015.
  */
 public class RegisterServletTest {
-    RegisterServlet servlet = mock(RegisterServlet.class);
+    RegisterServlet servlet = new RegisterServlet();
 
     private HttpServletRequest request;
     private HttpServletResponse response;
@@ -23,31 +26,46 @@ public class RegisterServletTest {
     private ServletContext context;
     private UserService service;
 
-    @Before
-    public void setUp() throws Exception {
-        request = mock(HttpServletRequest.class);
-        response = mock(HttpServletResponse.class);
-        context = mock(ServletContext.class);
-        service = new UserService(new InMemoryUserDaoImpl());
-    }
-
-    @Test
-    public void testDoPostValid() throws Exception {
-        when(context.getAttribute("userService")).thenReturn(service);
+    private void initUser() {
         when(request.getParameter("email")).thenReturn("apfhk@nfilj.ru");
         when(request.getParameter("name")).thenReturn("vlad");
         when(request.getParameter("password")).thenReturn("iufhwiu");
     }
 
-    @Test
-    public void testForNullValidation() throws Exception {
+    @Before
+    public void setUp() throws Exception {
+        request = mock(HttpServletRequest.class);
+        response = mock(HttpServletResponse.class);
+        context = mock(ServletContext.class);
+        service = mock(UserService.class);
+
+        when(request.getServletContext()).thenReturn(context);
         when(context.getAttribute("userService")).thenReturn(service);
-//        when(response.get)
-        verify(response.getWriter()).append(anyString());
+        when(context.getAttribute("postgreDao")).thenReturn(mock(PostrgeUserDAO.class));
     }
 
     @Test
-    public void testForNullResponseAppend() throws Exception {
+    public void testDoPostValid() throws Exception {
+        initUser();
+        servlet.doPost(request, response);
+        verify(service).addUser(any(User.class));
+    }
 
+    @Test
+    public void testForNullValidation() throws Exception {
+        PrintWriter writer = (mock(PrintWriter.class));
+        when(response.getWriter()).thenReturn(writer);
+        servlet.doPost(request, response);
+        verify(response.getWriter(), atLeastOnce()).append(anyString());
+    }
+
+    @Test
+    public void testForException() throws Exception {
+        initUser();
+        when(response.getWriter()).thenReturn(mock(PrintWriter.class));
+        doThrow(new ServiceException()).when(service).addUser(any(User.class));
+
+        servlet.doPost(request, response);
+        verify(response.getWriter()).append(anyString());
     }
 }
